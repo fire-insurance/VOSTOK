618aa64f1b4a660729b62ac48f94b1389befc8da
Dmitry Duda <dmitrij.snake2000@yandex.ru>
2022-06-22 01:01:34 +1000

    metrics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
 
@@ -8,8 +9,9 @@ export default function Document() {
                 <meta charSet="UTF-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="robots" content="index,follow" />
-		<meta name="yandex-verification" content="c402569a83f930dc" />
+                <meta name="yandex-verification" content="c402569a83f930dc" />
                 <link rel="icon" href="/favicon.ico" />
+                <Script src='/metrics.js' strategy='beforeInteractive'></Script>
             </Head>
             <body>
                 <Main />
